e9212dc1643a6df621f82f955d38cef101e36cd4
Saiesh <saieshkamble1@gmail.com>
2023-07-28 20:43:48 +0530

    new post screen
    Generated by create-expo-app 2.0.3.

--- app/(tabs)/Post.tsx
+++ app/(tabs)/Post.tsx
@@ -1,12 +1,41 @@
-import { StyleSheet } from 'react-native';
+import {Pressable, StyleSheet, TextInput} from 'react-native';
 
 //import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import {useNavigation, useRouter} from "expo-router";
+import {useLayoutEffect, useState} from "react";
 
 export default function PostScreen() {
+
+  const [content, setContent] = useState('');
+
+  const navigation = useNavigation();
+  const router = useRouter();
+const postButton = () => {
+  console.warn('posted ', content);
+  router.push('/(tabs)/');
+  // setContent('');
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'New Post',
+      headerRight: () =>
+          <Pressable onPress={postButton} style={styles.Button}>
+            <Text style={styles.ButtonText}>Post</Text>
+          </Pressable>
+    });
+  }, [postButton])
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Post</Text>
+      <TextInput
+          placeholder=" lets add input"
+          style={styles.input}
+          multiline
+          value={content}
+          onChangeText={setContent}
+ />
+
 
     </View>
   );
@@ -15,16 +44,39 @@ export default function PostScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+     padding: 15,
+
+
+
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  input: {
+    backgroundColor: 'white',
+    fontSize: 19,
+    fontWeight: '500',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  Button: {
+    padding: 8,
+    backgroundColor: 'royalblue',
+    borderRadius: 50,
+    paddingHorizontal: 30,
+    alignItems: "center",
+    // paddingRight: 10,
+    marginRight: 10,
+
+
   },
+  ButtonText: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: "bold"
+  }
+  // title: {
+  //   fontSize: 20,
+  //   fontWeight: 'bold',
+  // },
+  // separator: {
+  //   marginVertical: 30,
+  //   height: 1,
+  //   width: '80%',
+  // },
 });
