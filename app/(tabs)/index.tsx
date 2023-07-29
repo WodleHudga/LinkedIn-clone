7327658b42ba98fc6f87d744896c6764d96c46c1
Saiesh <saieshkamble1@gmail.com>
2023-07-29 21:21:08 +0530

    getting data in the home page in a paginated form Generated by create-expo-app 2.0.3.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { Text, FlatList, ActivityIndicator} from 'react-native';
 import PostListItem from '../../components/PostListItem';
 import {gql, useQuery} from "@apollo/client";
-import React from "react";
+import React, {useState} from "react";
 //import posts from '../../assets/data/posts.json';
 
 
@@ -21,29 +21,65 @@ const postList = gql`
     }
 `;
 
+const postPaginated = gql`
+    query postPaginatedList($first: Int, $after: Int) {
+        postPaginatedList(first: $first, after: $after) {
+            id
+            content
+            image
+            profile {
+                id
+                name
+                image
+                position
+            }
+        }
+    }`;
+
 //const firstPost = posts[0];
 export default function Home() {
+    const [hasmore, setHasmore] = useState(true);
 
-    const {loading, error, data} = useQuery(postList);
+    const {loading, error, data, fetchMore} = useQuery(postPaginated, {
+        variables: {
+            first: 2
+        }
+    });
 
-    if(loading){
-        return <ActivityIndicator />;
+    if (loading) {
+        return <ActivityIndicator/>;
     }
-    if(error){
+    if (error) {
         console.log(error);
         return <Text>something went wrong!</Text>
     }
     console.log(data);
-  return (
-   <FlatList
-       data={data.postList}
-       renderItem={({item}) =>
-         <PostListItem post={item} />
-       }
-       showsVerticalScrollIndicator={false}
-       contentContainerStyle={{gap: 5}}
-   />
-  );
+    const loadMore = async () => {
+        if (!hasmore) {
+            return;
+        }
+        const res = await fetchMore(
+            {variables: {after: data.postPaginatedList.length}}
+        );
+        if (res.data.postPaginatedList.length === 0) {
+            setHasmore(false);
+        }
+    };
+    return (
+        <FlatList
+            data={data.postPaginatedList}
+            renderItem={({item}) =>
+                <PostListItem post={item}/>
+            }
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{gap: 5}}
+            onEndReached={loadMore}
+            //     ListFooterComponent={() => (
+            //         <Text onPress={loadMore} style={{alignSelf: 'center', fontWeight: '400', fontSize: 15, color: 'royalblue'}}>Load More</Text>)}
+            // />
+            // );
+            //}
+        />
+    );
 }
-
 
--- appolo/client.tsx
+++ appolo/client.tsx
@@ -1,10 +1,24 @@
 // Container component
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {ApolloClient, InMemoryCache, TypePolicies} from '@apollo/client';
+
+
+const typePolicies: TypePolicies = {
+    Query: {
+        fields: {
+            postPaginatedList:{
+                keyArgs: false,
+                merge(existing = [], incoming) {
+                    return [...existing, ...incoming]
+}
+            },
+        }
+    }
+}
 
 const client = new ApolloClient({
     uri: 'https://bukedea.stepzen.net/api/oldfashioned-otter/__graphql',
     headers: {'Authorization':'apikey bukedea::stepzen.io+1000::2950b13db206ba53288c66861249e9b4f6bef5fb9589da710e0b0e8f42c00109'},
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({typePolicies}),
 });
 
 export default client;
